6d2781f6d6c7c89ef57f6917e05124d644cd5bf9
Aleks <alexandr.kutuyev.101@gmail.com>
2021-06-17 16:20:17 +0300

    new task

---
+++ HT 6/js/script.js
@@ -0,0 +1,25 @@
+// Task 1
+    Boolean(Number('10')) + 1; // 2
+    'sub' + Number(false); //sub0
+    16  *  '      91    ' // 1456
+    true - 70; // -69
+    Number(1 + String(1)) + 1; // 12
+
+// Task 2
+    let hours = prompt('Введите ваше количество часов');
+    const sec = hours * 3600;
+
+    alert(`Количесво секунд: ${sec}с`);
+
+// Task 3
+    let num = 1;
+
+    num += 12;
+    num -= 14;
+    num *= 5;
+    num /= 7;
+    num++;
+    num--;
+    
+    alert(num);
+
