edad4213a70521338d89a541ac0789ea0c825699
Aleks <alexandr.kutuyev.101@gmail.com>
2021-06-27 22:01:52 +0300

    update

--- HT 12/js/script.js
+++ HT 12/js/script.js
@@ -1,1 +1,16 @@
-    let num = prompt('Введите ваше число');
+    let num = Number(prompt('Введите ваше число'));
+
+    if (isNaN(num)) {
+        alert(NaN);
+    } else if (num < 1) {
+        alert(NaN);
+    } else if (num === 1 || num === 2) {
+        alert('Ваше число простое');
+    } else {
+        for (let i = 2; i < num; i++) {
+            if (num % i === 0) {
+                alert(`Наименьший делитель числа: ${i}`);
+                break;
+            }
+        }
+    }
