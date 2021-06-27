78c9ff14b6c1c31f11862aebb4f82dd4961a67f3
Aleks <alexandr.kutuyev.101@gmail.com>
2021-06-27 22:03:28 +0300

    update

--- HT 12/js/script.js
+++ HT 12/js/script.js
@@ -4,9 +4,7 @@
         alert(NaN);
     } else if (num < 1) {
         alert(NaN);
-    } else if (num === 1 || num === 2) {
-        alert('Ваше число простое');
-    } else {
+    }  else {
         for (let i = 2; i < num; i++) {
             if (num % i === 0) {
                 alert(`Наименьший делитель числа: ${i}`);
