f3d3c436aa5693926f4299b978f4535445eec2ff
Aleks <alexandr.kutuyev.101@gmail.com>
2021-07-16 11:45:44 +0300

    update

---
+++ HT 23/js/script.js
@@ -0,0 +1,37 @@
+// factorial
+    /*function factorial(number) {
+        if (number === 1) return number;
+
+        return number * factorial(number - 1);
+    }
+
+    console.log(factorial(5));
+    console.log(factorial(7));
+    console.log(factorial(3));*/
+
+// pow
+    /*function pow(number, degree) {
+        if (degree === 1) return number;
+
+        return number * pow(number, degree - 1);
+    }
+
+    console.log(pow(2, 3));
+    console.log(pow(5, 2));
+    console.log(pow(7, 5));
+*/
+
+/*
+// sum
+    function sum(firstNum, secondNum) {
+        firstNum += 1;
+        secondNum -= 1;
+
+        if (secondNum === 0) return firstNum;
+
+        return sum(firstNum, secondNum);
+    }
+
+    console.log(sum(5, 7));
+    console.log(sum(17, 34));
+    console.log(sum(15, 8));*/
